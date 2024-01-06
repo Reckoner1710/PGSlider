6ed35bb63377d105a8546fe14548848814a25409
Puneet <puneet.gurtoo0@gmail.com>
2024-01-06 12:45:25 +0530

    Initial Commit

---
+++ SwiiftUI-ParentChild/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiiftUI-ParentChild
+//
+//  Created by Puneet Gurtoo on 06/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiiftUI-ParentChild/SwiiftUI_ParentChildApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiiftUI_ParentChildApp.swift
+//  SwiiftUI-ParentChild
+//
+//  Created by Puneet Gurtoo on 06/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiiftUI_ParentChildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
